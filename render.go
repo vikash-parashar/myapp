7c59ce7d7ce04a50cbe9449bbed22da67e710c23
Vikash Parashar <vikash@nexg.tech>
2023-09-06 03:04:22 +0530

    initial commit

--- render.go
+++ render.go
@@ -10,11 +10,11 @@ import (
 func renderTemplate(w http.ResponseWriter, name string, data any) {
 	tpl, err := template.ParseFiles("./templates/" + name + ".html")
 	if err != nil {
-		log.Fatalln("failed to parse files")
+		log.Fatalln("failed to parse ", strings.ToUpper(name), "file")
 		return
 	}
 	if err = tpl.Execute(w, data); err != nil {
-		log.Println("failed to execute template")
+		log.Println("failed to execute", strings.ToUpper(name), " template")
 		return
 	}
 	log.Println(strings.ToUpper(name) + " template executed successfully")
