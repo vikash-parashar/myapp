9922dea966346a1ee3e52dd4ac6082126f12e8ac
Vikash Parashar <vikash@nexg.tech>
2023-09-06 04:46:40 +0530

    added custom font style and body bf color for templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
-	log.Println("starting our application on port : ", port)
+	log.Println("starting our application on port ", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Println("failed to start application")
 		return
