36f37d178daa36be5dbdd0514435e1679e3f7e78
Vikash Parashar <vikash@nexg.tech>
2023-09-06 11:40:34 +0530

    using go-chi/chi routing packing for our routes to handle request

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,6 +13,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Logger)
+	mux.Use(WriteToConsole)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/vikash-parashar/myapp
 go 1.20
 
 require github.com/go-chi/chi v1.5.4
+
+require github.com/justinas/nosurf v1.1.1 // indirect
